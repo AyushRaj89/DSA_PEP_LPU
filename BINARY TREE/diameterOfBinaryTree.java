69569a57b2a96616dab3867058b84579d7800b5f
AyushRaj89 <151430844+AyushRaj89@users.noreply.github.com>
2026-02-19 13:22:23 +0530

    Implement diameter of binary tree algorithm

---
+++ BINARY TREE/diameterOfBinaryTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxdia = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return maxdia;
+    }
+
+    public int height(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        maxdia = Math.max(maxdia, left+right);
+
+        return 1 + Math.max(left, right);
+    }
+}
