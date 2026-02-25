c8735488c804e8a2603f1bdf4bc134a426789e2f
AyushRaj89 <151430844+AyushRaj89@users.noreply.github.com>
2026-02-25 06:41:59 +0530

    Implement BFS algorithm in bfs.java

---
+++ GRAPH/bfs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int v = adj.size();
+        boolean[] visited = new boolean[v];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.offer(0);
+        visited[0] = true;
+        
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            ans.add(node);
+            
+            for(int neighbour : adj.get(node)){
+                if(!visited[neighbour]){
+                    visited[neighbour] = true;
+                    q.offer(neighbour);
+                }
+            }
+        }
+        return ans;
+    }
+}
